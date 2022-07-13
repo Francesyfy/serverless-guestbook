f785fb0a43c2d1a9282bd4f9d80f20334b8a262d
Frances <43921467+Francesyfy@users.noreply.github.com>
2022-07-13 19:34:43 -0400

    add apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4d83bbb1.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
